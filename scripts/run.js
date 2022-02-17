f9b28048d54ae1b02c43830158b1a795ea6db5c2
TomiMoreno <tomimoreno03@gmail.com>
2022-02-16 23:33:07 -0300

    Initial contract

---
+++ scripts/run.js
@@ -0,0 +1,27 @@
+const main = async () => {
+  const nftContractFactory = await hre.ethers.getContractFactory("DoodleNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("DoodleNFT deployed to:", nftContract.address);
+
+  let txn = await nftContract.makeADoodle();
+
+  await txn.wait();
+
+  console.log("Doodle created");
+  console.log(txn);
+
+  txn = await nftContract.makeADoodle();
+  await txn.wait();
+};
+
+const runMain = async () => {
+  try {
+    await main();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
